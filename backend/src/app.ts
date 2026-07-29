a98d223fb94b6ad74abaeff0ade9d4297d4599ea
triggereddown <deepmoitra2@gmail.com>
2026-07-30 00:03:58 +0530

    implemented my routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -31,7 +31,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://analytify.vercel.app"],
+    origin: ["http://localhost:5173", "https://analytify.vercel.app", "https://analytify.deepmoitra.dev"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
